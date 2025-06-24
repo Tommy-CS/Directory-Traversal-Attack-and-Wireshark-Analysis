c7e6c1e65d793c03adcf1ea598ac6d2a1a989a4e
Tommy-CS <138081263+Tommy-CS@users.noreply.github.com>
2025-06-23 22:58:13 -0500

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -11,22 +11,16 @@ NC='\033[0m' # No Color
 
 ### Step 1: Start the server
 echo -e "\t[${GREEN}start vulnerable server${NC}]: ${BLUE}hftp${NC}"
-
 node scripts/start-server.js &
-
 vulnpid=$!
 
 ### Step 2: Wait for the server to get started
-
 sleep 2
-
 echo -e "\t[${GREEN}server root directory${NC}]: `pwd`"
 
 ### Step 3: Perform directory attack
-
 ATTACK_PATH = "http://localhost:8888/general/reports.txt"
-
 node attack.js $ATTACK_PATH
-### Step 4: Clean up the vulnerable npm package's process
 
+### Step 4: Clean up the vulnerable npm package's process
 kill -9 $vulnpid
